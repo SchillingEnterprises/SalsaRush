8b93c4511bb6f95631491bf6f6ac8d3167d00115
Gavin Clay Schilling <schillgc@users.noreply.github.com>
2024-10-17 08:17:31 -0400

    Initial Commit

--- salsaRush/salsaRush/settings.py
+++ salsaRush/salsaRush/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,5 @@ STATIC_URL = 'website/static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- salsaRush/website/urls.py
+++ salsaRush/website/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 
 from . import views
 
-app_name = "polls"
+app_name = "website"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("about", views.about, name="about"),
+    path("contact", views.contact, name="contact"),
+    path("event_list", views.event_list, name="event_list"),
+    path("event_form", views.event_form, name="event_form"),
 ]
